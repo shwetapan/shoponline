60a41fe18cf546d4ab455c1125893575eb34102c
Shweta Pandey <shwetapandey9207@gmail.com>
2021-09-21 19:38:30 +0530

    updated static dir path

--- shoponline/settings.py
+++ shoponline/settings.py
@@ -129,6 +129,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+]
 LOGIN_REDIRECT_URL = '/profile/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
