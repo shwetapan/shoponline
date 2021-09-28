21886ff9bfe02b9fff5d3318b404597fce22bb57
Shweta Pandey <shwetapandey9207@gmail.com>
2021-09-28 13:56:51 +0530

    Update settings.py

--- shoponline/settings.py
+++ shoponline/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['shop--online.herokuapp.com','127.0.0.1']
 
